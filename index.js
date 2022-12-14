de27c37e087b5020f5958f6c058fe6979cd463d5
Natali Isla <natali.isla.njib@gmail.com>
2022-12-14 03:03:17 -0500

    initial

--- index.js
+++ index.js
@@ -12,27 +12,3 @@ while (!end) {
   end = confirm('Eso es todo?');
   console.log(services);
 }
-
-console.log('TODOS', services);
-
-function express(packageQuantity) {
-    let cost = 100;
-  
-    if (packageQuantity > 60) {
-      cost = 2 * packageQuantity * 100;
-    }
-  
-    alert(`Tu costo es ${cost}`);
-  }
-  
-  function nextDay() {
-    console.log(0);
-  }
-  
-  switch (prompt('Que servicio quieres?')) {
-    case 'Express':
-      express();
-      break;
-    case 'NextDay':
-      nextDay();
-  }
